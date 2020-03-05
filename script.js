33962ad26c3e0d0821035c251ace7a8bcd884b5d
kautsar_r <49815027+kautsarR@users.noreply.github.com>
2020-03-05 14:49:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.body.addEventListener('touchmove', function (event) {
  const y = Math.round((event.touches[0].clientY / window.innerWidth) * 255);
  const b = Math.round((event.touches[1].clientY / window.innerWidth) * 255);
  let b = Math.round((event.touches[1].clientY / window.innerWidth) * 255);
- if (b = 0) {
+ if (b == 0) {
   b = 100;
  }
  document.body.style.backgroundColor = 'rgb(' + x + ',' + y + ', '+ b +')';
