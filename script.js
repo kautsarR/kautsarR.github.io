c743dc11a92eb54eab29af1162c3f30118970ffc
kautsar_r <49815027+kautsarR@users.noreply.github.com>
2020-03-05 14:47:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.body.addEventListener('touchmove', function (event) {
  const x = Math.round((event.touches[0].clientX / window.innerWidth) * 255);
  const y = Math.round((event.touches[0].clientY / window.innerWidth) * 255);
  const b = Math.round((event.touches[1].clientY / window.innerWidth) * 255);
+ let b = Math.round((event.touches[1].clientY / window.innerWidth) * 255);
  if (b = 0) {
   b = 100;
  }
